48797e58c180dbb6e4cea7079dc8875c3f6adbf9
dikih arif wibowo <dikih.wibowo@dana.id>
2022-06-27 11:15:20 +0700

    day1 string data type

---
+++ build/day1-string-data-type.js
@@ -0,0 +1,7 @@
+"use strict";
+let fullName = "John Doe";
+fullName = "Diki Arif Wibowo";
+console.log(fullName);
+// Output: Diki Arif Wibowo
+fullName = 23;
+// Output: Error: Type '23' is not assignable to type 'string'.
---
+++ src/basic-typescript/day1-string-data-type.ts
@@ -0,0 +1,8 @@
+let fullName = "John Doe";
+
+fullName = "Diki Arif Wibowo";
+console.log(fullName)
+// Output: Diki Arif Wibowo
+
+fullName = 23
+// Output: Error: Type '23' is not assignable to type 'string'.
