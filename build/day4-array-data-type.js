e02b4080553b94de963784f4f1132892922b8aa5
dikih arif wibowo <dikih.wibowo@dana.id>
2022-06-30 21:18:31 +0700

    day 5: array tuple

--- build/day4-array-data-type.js
+++ build/day4-array-data-type.js
@@ -3,12 +3,12 @@
 let array;
 array = [1, 2, 3, 4, 5];
 // Output: [1, 2, 3, 4, 5]
-array = [1, 2, 3, 4, 5, "string"];
+//array = [1, 2, 3, 4, 5, "string"];
 // Output: Type 'string' is not assignable to type 'number'.
 let array2;
 array2 = ["string 1", "string 2", "string 3"];
 // Output: ["string 1", "string 2", "string 3"]
-array2 = [1, 2, 3, 4, 5];
+//array2 =[1, 2, 3, 4, 5];
 // Output: Type 'number' is not assignable to type 'string'.
 let array3;
 array3 = [1, 2, 3, "string", true, { name: "John" }];
--- src/basic-typescript/day4-array-data-type.ts
+++ src/basic-typescript/day4-array-data-type.ts
@@ -3,13 +3,13 @@
 let array: number[];
 array = [1, 2, 3, 4, 5];
 // Output: [1, 2, 3, 4, 5]
-array = [1, 2, 3, 4, 5, "string"];
+//array = [1, 2, 3, 4, 5, "string"];
 // Output: Type 'string' is not assignable to type 'number'.
 
 let array2: string[];
 array2 = ["string 1", "string 2", "string 3"];
 // Output: ["string 1", "string 2", "string 3"]
-array2 =[1, 2, 3, 4, 5];
+//array2 =[1, 2, 3, 4, 5];
 // Output: Type 'number' is not assignable to type 'string'.
 
 let array3: any[];
